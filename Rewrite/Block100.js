ae0de6751dcc072af925b0ef568454c1a9a79bd4
KyleNiu0x00 <105152200+KyleNiu0x00@users.noreply.github.com>
2022-11-03 21:59:33 +0800

    Create Block100.js

---
+++ Rewrite/Block100.js
@@ -0,0 +1,6 @@
+# > 拦截100 3.0.2
+var body = $response.body;
+body = "lvCQG8cCxqficLk+LttK+OvjY+kGEoGHRWop15GMRVg1TU8oQTFHsNCJIEJMEYYfDjqpfM0sxeXRILHsoullvHqzmN6X7HmMRHqOjr3G0AXp2FtlU91l2+2ZbtUpL8p2cc6Y6JdCOUiADpqc4GZktNpGoED1rMVltIjdbhLGVgO0tYaNtQ/dV52tpmn+Lcm+/3pCU8/wXdnCfkkMB0QZc6psJavFUF6dLfDRzagLuxiwgOQmNQraUG99e4YLDmoQ";
+$done({ 
+    body 
+});
