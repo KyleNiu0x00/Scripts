487d086a9e157322ef331e97f7aba4d6d14e5c6c
KyleNiu0x00 <105152200+KyleNiu0x00@users.noreply.github.com>
2026-06-21 04:49:37 +0800

    更新 wzzyy.js

--- Rewrite/wzzyy.js
+++ Rewrite/wzzyy.js
@@ -1,10 +1,14 @@
 *******************************
+https://apps.apple.com/id6465685823
+https://apps.apple.com/id6755513958
+
+
 [rewrite_local]
 
-^http:\/\/member\.honghukeji\.work\/api\/user url script-response-body 
+^http:\/\/member\.honghukeji\.work\/api\/user url script-response-body https://raw.githubusercontent.com/KyleNiu0x00/Scripts/refs/heads/main/Rewrite/wzzyy.js
 
 
-^http:\/\/member\.lizhikeji\.com\/api\/user url script-response-body 
+^http:\/\/member\.lizhikeji\.com\/api\/user url script-response-body https://raw.githubusercontent.com/KyleNiu0x00/Scripts/refs/heads/main/Rewrite/wzzyy.js
 
 
 
